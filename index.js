1e7eb9ab61b640045023d814b8efba02c62ac4ba
Hao Zheng <Hao@d-108-179-145-239.dhcp4.washington.edu>
2014-10-18 09:09:41 -0700

    Add more test

--- index.js
+++ index.js
@@ -37,8 +37,15 @@ app.post('/sms', function(req, res) {
 		//var msg = req.body;
  		//var from = req.body.From;	
 
+		console.log(req);
+
+		//iswitch = new Switch({
+	    //switchMsg:req;
+
+	   // iswitch.save();
+
+	//  });
   
-		console.log(req.body);
 
 });
 
